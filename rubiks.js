3d2eed26908cdc20befd2bf042002b1842ca77df
GiftGiwa <gdg2131@columbia.edu>
2024-05-16 00:40:11 -0400

    restrict orbit controls to when cube isn't being hovered over AND the mouse isn't already held down

--- rubiks.js
+++ rubiks.js
@@ -2,6 +2,7 @@ import * as THREE from 'three'
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js'
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js'
 import { TrackballControls } from 'three/addons/controls/TrackballControls.js'
+import { DragControls } from 'three/addons/controls/DragControls.js'
 
 
 const scene = new THREE.Scene();
@@ -13,7 +14,7 @@ const camera = new THREE.PerspectiveCamera(
 );
 camera.position.x = 0
 camera.position.y = 0
-camera.position.z = 0.2
+camera.position.z = 0.15
 
 
 const renderer = new THREE.WebGLRenderer({
@@ -26,28 +27,27 @@ renderer.setSize(
 )
 
 
-document.body.appendChild( renderer.domElement )
+document.body.appendChild(renderer.domElement)
+// console.log(renderer.domElement)
 
 // resize render on window resize
 window.addEventListener( 'resize', onWindowResize, false )
 
 function onWindowResize() {
-
     camera.aspect = window.innerWidth / window.innerHeight
     camera.updateProjectionMatrix()
-    renderer.setSize( window.innerWidth, 500 )
-
+    renderer.setSize(window.innerWidth, 500)
 }
 
 const orbitControls = new OrbitControls( camera, renderer.domElement )
 orbitControls.minDistance = 0.15
-orbitControls.maxDistance = 0.5
+orbitControls.maxDistance = 0.3
 
 const ambientLight = new THREE.AmbientLight( 0x404040 ); // soft white light
 scene.add( ambientLight );
 
 const axesHelper = new THREE.AxesHelper( 5 );
-scene.add( axesHelper );
+// scene.add( axesHelper );
 
 const lightPositions = [
     [0.5, 0.5, 0.5], 
@@ -91,18 +91,49 @@ loader.load( './public/assets/models/rubiks.gltf', function ( gltf ) {
         console.error( error )
 });
 
-
-
 const raycaster = new THREE.Raycaster();
 const pointer = new THREE.Vector2();
 
 console.log(scene.children)
 
 
+function onPointerMove( event ) {
+	pointer.x = ( event.clientX / window.innerWidth ) * 2 - 1;
+	pointer.y = - ( event.clientY / window.innerHeight ) * 2 + 1;
+}
 
+window.addEventListener('pointermove', onPointerMove)
+
+
+
+
+let mouseDown = false
+document.body.onmousedown = function () {
+    mouseDown = true
+}
+document.body.onmouseup = function () {
+    mouseDown = false
+}
 
 
 function animate() {
+    raycaster.setFromCamera( pointer, camera );
+
+    const intersects = raycaster.intersectObjects( scene.children );
+
+    // block orbit controls if the cube is being hovered over
+    if (!mouseDown) {
+        if (intersects.length > 0) {
+            orbitControls.enabled = false
+        } else {
+            orbitControls.enabled = true
+        }
+    }
+
+
+	
+
+
 	requestAnimationFrame( animate )
 	renderer.render( scene, camera )
 }
