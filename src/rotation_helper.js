aef5c6024427a3a2fbaf67ca855aacf95085dc17
Gift Giwa <gdg2131@columbia.edu>
2025-08-26 00:10:26 -0500

    write in and update comments in RotationHelper class

--- src/rotation_helper.js
+++ src/rotation_helper.js
@@ -1,5 +1,6 @@
 import * as THREE from 'three'
 import RubiksPiece from './pieces'
+import RubiksCube from './rubiks'
 
 /**
  * Class for handling updates of coordinates and coordinate maps for individual
@@ -7,6 +8,16 @@ import RubiksPiece from './pieces'
  */
 class RotationHelper {
 
+    /**
+     * For a given face and direction, modifies the stored groups for the
+     * Rubik's cube's pieces to reflect the cube's side's physical rotation.
+     * @param {RubiksCube} rubiksCube 
+     * @param {string} direction direction the side of the Rubik's Cube rotates -
+     *                      either "cw" (clockwise) or "ccw" (counterclockwise)
+     * @param {string} color color of face piece that rotates -
+     *                       either "R" (red), "O" (orange), "Y" (yellow),
+     *                       "G" (green), "B" (blue), or "W" (white)
+     */
     static rotateFace(rubiksCube, direction, color) {
         let rotationMap = null
         if (direction == "ccw")
@@ -21,7 +32,7 @@ class RotationHelper {
                 piece.mesh.rotateOnWorldAxis(rubiksCube.rotationAxes[color], -Math.PI / 2)
 
         for (let piece of rubiksCube.rotationGroups[color]) {
-            if (piece.colors.length == 3) { // handling corners
+            if (piece.colors.length == 3) { // handling corner
                 for (let i = 0; i < rotationMap[color].length; i++) {    
                     let sourceFace = rotationMap[color][i]
 
@@ -39,11 +50,11 @@ class RotationHelper {
                     
                     if (rubiksCube.rotationGroups[sourceFace].includes(piece) 
                         && rubiksCube.rotationGroups[adjacentFace].includes(piece)) {
-                        this.rotatePiece(rubiksCube, piece, sourceFace, destinationFace)
+                        this.transferPiece(rubiksCube, piece, sourceFace, destinationFace)
                         break
                     }
                 }
-            } else if (piece.colors.length == 2) { // handling edges
+            } else if (piece.colors.length == 2) { // handling edge
                 for (let i = 0; i < rotationMap[color].length; i++) {
                     let sourceFace = rotationMap[color][i]
 
@@ -54,18 +65,33 @@ class RotationHelper {
                         destinationFace = rotationMap[color][0]
 
                     if (rubiksCube.rotationGroups[sourceFace].includes(piece)) {
-                        this.rotatePiece(rubiksCube, piece, sourceFace, destinationFace)
+                        this.transferPiece(rubiksCube, piece, sourceFace, destinationFace)
                         break
                     }
                 }
-            } else // handling center piece – do nothing
+            } else // handling center piece of face – do nothing
                 continue
         }
         console.log(rubiksCube.rotationGroups)
     }
 
-    static rotatePiece(rubiksCube, piece, sourceFace, destinationFace) {
-        // remove the piece that already exists
+    /**
+     * Helper function that transfers a given RubikePiece object from one
+     * face's array to another face's array
+     * @param {RubiksCube} rubiksCube Rubiks Cube object whose rotation groups are
+     *                       referenced in the implementation
+     * @param {RubiksPiece} piece piece to be transferred from one face array to another
+     * @param {string} sourceFace original color of face whose array the piece
+     *                            belongs to - either "R" (red), "O" (orange),
+     *                            "Y" (yellow), "G" (green), "B" (blue), or
+     *                            "W" (white)
+     * @param {string} destinationFace color of face whose array the piece gets
+     *                                 moved to - either "R" (red), "O"
+     *                                 (orange), "Y" (yellow), "G" (green),
+     *                                 "B" (blue), "W" (white)
+     */
+    static transferPiece(rubiksCube, piece, sourceFace, destinationFace) {
+        // remove the piece that already exists in the source face array
         for (let i = rubiksCube.rotationGroups[sourceFace].length - 1; i > -1; i--) {
             let currentPiece = rubiksCube.rotationGroups[sourceFace][i]
             if (currentPiece == piece) {
@@ -73,6 +99,8 @@ class RotationHelper {
                 break
             }
         }
+
+        // add the piece from to the destination face array
         rubiksCube.rotationGroups[destinationFace].push(piece)
     }
 
