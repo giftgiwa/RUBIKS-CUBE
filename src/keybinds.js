1c18294303a51d3232fc5265179ecaeaa67093de
Gift Giwa <gdg2131@columbia.edu>
2025-08-26 02:20:30 -0500

    remove console log statement

--- src/keybinds.js
+++ src/keybinds.js
@@ -3,21 +3,7 @@ import RotationHelper from './rotation_helper'
 class KeybindHelper {
 
     static addInputs(rubiksCube) {
-        console.log("addInputs()")
 
-        // window.addEventListener("keypress", (event) => {
-    //         if (event.key.toLowerCase() == "r") {
-    //             // this.rotateFace("cw", "B")
-    //             RotationHelper.rotateFace(this, "cw", "B")
-    //         }
-    //     })
-
-    //     window.addEventListener("keypress", (event) => {
-    //         if (event.key.toLowerCase() == "d") {
-    //             // this.rotateFace("ccw", "W")
-    //             RotationHelper.rotateFace(this, "ccw", "W")
-    //         }
-    //     })
         let colors = new Set(["r", "o", "y", "g", "b", "w"])
         window.addEventListener("keypress", (event) => {
             if (colors.has(event.key.toLowerCase())) {
