085ca7ce747ad2197c030c0c9fda7e8ba301842d
Gift Giwa <gdg2131@columbia.edu>
2025-08-26 00:14:31 -0500

    partially complete comments in RubiksPiece class

--- src/pieces.js
+++ src/pieces.js
@@ -1,4 +1,19 @@
+/**
+ * Class that represents piece on Rubik's cube.
+ */
 class RubiksPiece {
+
+    /**
+     * Create a Rubiks Piece object.
+     * @param {*} colors list of colors on the Rubik's Cube piece - either three
+     *                   (corner piece), two (edge piece), or one (face piece)
+     *                   unique ones from the following: "R" (red), "O"
+     *                   (orange), "Y" (yellow), "G" (green), "B" (blue), or
+     *                   "W" (white) 
+     * @param {*} coordinates Three-number array
+     * @param {*} orientationMap Hash-map that represents orientation
+     * @param {*} mesh 
+     */
     constructor(colors, coordinates, orientationMap, mesh) {
         this.colors = colors
         this.coordinates = coordinates // coordinates dictates position
@@ -6,6 +21,5 @@ class RubiksPiece {
         this.mesh = mesh // mesh of individual piece on actual cube
     }
 }
-
 
 export default RubiksPiece
