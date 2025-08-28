a44a1198b5a8e8a43fe608f2338ff36dab959b02
GiftGiwa <gdg2131@columbia.edu>
2025-08-28 17:32:08 -0500

    update imports in keybinds.js, rename KeybindHelper class to Keybinds class

--- src/keybinds.js
+++ src/keybinds.js
@@ -1,6 +1,6 @@
-import RotationHelper from './rotation_helper'
+import RotationHelper from './RotationHelper'
 
-class KeybindHelper {
+class Keybinds {
 
     static addInputs(rubiksCube) {
 
@@ -17,4 +17,4 @@ class KeybindHelper {
 
 }
 
-export default KeybindHelper
+export default Keybinds
