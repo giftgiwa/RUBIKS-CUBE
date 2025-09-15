908bf6a836457ee89a326280f6c2a9e277884b2b
GiftGiwa <gdg2131@columbia.edu>
2025-09-15 02:37:59 -0500

    log position and colors of three neighboring corners on rubiks cube using helper functions

--- src/rubiks-animation.js
+++ src/rubiks-animation.js
@@ -5,18 +5,14 @@ import { OrbitControls } from 'three/addons/controls/OrbitControls.js'
 
 class RubiksAnimationHelper {
 
-	constructor(rubiksCube) {
+	constructor(rubiksCube, camera, renderer) {
 		this.rubiksCube = rubiksCube
-		console.log(this.rubiksCube)
-		console.log(this.rubiksCube.corners)
+		this.camera = camera
+		this.renderer = renderer
 	}
 
 	setupAnimation(rubiksCube, intersects) {
 		// TODO: add click and drag for rotation
-		
-	
-
-
 	}
 
 	/**
@@ -24,7 +20,57 @@ class RubiksAnimationHelper {
 	 * vectors used to detect faces being rotated from each view of the cube
 	 */
 	getpCornerVectors() {
-		console.log("getCornerVectors()")
+
+		for (let i = 0; i < this.rubiksCube.corners.length; i++) {
+			for (let j = 0; j < this.rubiksCube.corners.length; j++) {
+				
+				if (i != j) {
+					if (this.areNeighbors(
+						this.rubiksCube.corners[i].colors,
+						this.rubiksCube.corners[j].colors
+					)) {
+						console.log(this.rubiksCube.corners[i].colors)
+						console.log(this.get2DPosition(this.rubiksCube.corners[i]))
+						console.log(this.rubiksCube.corners[j].colors)
+						console.log(this.get2DPosition(this.rubiksCube.corners[j]))
+					}
+					
+				}
+			}
+			break
+		}
+	}
+
+	areNeighbors(colorsA, colorsB) {
+		let hashmapA = {}
+		let hashmapB = {}
+		for (let i = 0; i < 3; i++) {
+			hashmapA[colorsA[i]] = 1
+			hashmapB[colorsB[i]] = 1
+		}
+
+		let numCommonKeys = 0
+		for (const [key, value] of Object.entries(hashmapA)) {
+			if (key in hashmapB)
+				numCommonKeys++
+		}
+
+		return numCommonKeys == 2
+	}
+
+	get2DPosition(rubiksPiece) {
+		/**
+		 * Sample code for getting an object's position in 3d space and translating it
+		 * to 2D space (relative to renderer)
+		 * Source: https://stackoverflow.com/a/27412386
+		 */
+		let vector = new THREE.Vector3()
+		vector.setFromMatrixPosition(rubiksPiece.mesh.matrixWorld)
+		vector.project(this.camera)
+		vector.x = (vector.x + 1) * this.renderer.domElement.width / 2
+		vector.y = (-vector.y + 1) * this.renderer.domElement.height / 2
+
+		return vector
 	}
 
 }
