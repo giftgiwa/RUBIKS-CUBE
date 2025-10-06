7f994f4728889ef71808f5659c37690149cbc36a
GiftGiwa <gdg2131@columbia.edu>
2025-10-05 19:30:42 -0500

    add sample tile to cube map (just for visual testing )

--- src/cube-map.js
+++ src/cube-map.js
@@ -17,8 +17,24 @@ class CubeMap {
 		this.outerDiv.style.right = "10px"
 		this.outerDiv.style.bottom = "10px"
 		this.outerDiv.style.backdropFilter = "blur(8px)"
-		
+		this.outerDiv.style.padding = "10px";
 
+		let sampleTile = document.createElement("div")
+
+		sampleTile.style.borderWidth = "2px"
+		sampleTile.style.borderRadius = "2px"
+		sampleTile.style.borderColor = "black"
+		sampleTile.style.width = "20px"
+		sampleTile.style.height = "20px"
+		sampleTile.style.position = "relative"
+		sampleTile.style.margin = "0"
+		sampleTile.style.backgroundColor = "#fff"
+
+
+
+
+		
+		this.outerDiv.appendChild(sampleTile)
 
 		document.body.appendChild(this.outerDiv)
 
